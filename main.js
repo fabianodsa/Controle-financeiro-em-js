378192e9c0355753b0253c5a2aea0c7062d6defb
fabianodsa <fabyanoduarte@hotmail.com>
2022-12-18 21:19:25 -0300

    Função que adiciona transação criada

--- main.js
+++ main.js
@@ -1,18 +1,27 @@
+const transactionsUl = document.querySelector('#transactions')
+
 const dummyTransactions = [
     { id: 1, name: 'Bolo de brigadeiro', amount: -20},
     { id: 2, name: 'Salário', amount: 300},
     { id: 3, name: 'Torta de frango', amount: -10},
     { id: 4, name: 'Violão', amount: 150}
 ]
-// função para adicionar a transação no DOM. 
-//O parâmetro "transaction recebe um objeto que representa a transação"
+
 const adicionarTransacao = transaction => {
     const operador = transaction.amount < 0 ? '-' : '+'
-    console.log(operador)
+    const CSSClass = transaction.amount < 0 ? 'minus' : 'plus'
+    const valorSemOperador = Math.abs(transaction.amount)
+    const li = document.createElement('li')
 
-    // <li class="minus">
-    // Salário <span>-$400</span><button class="delete-btn">x</button>
-    // </li>
+    li.classList.add(CSSClass)
+    li.innerHTML = `
+        ${transaction.name} <span> ${operador} R$ ${valorSemOperador} </span><button class="delete-btn">x</button>
+    `
+    transactionsUl.prepend(li)
 }
 
-adicionarTransacao(dummyTransactions[1])
+const init = () => {
+    dummyTransactions.forEach(adicionarTransacao)
+}
+
+init()
