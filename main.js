c70de6705b899074054b18d64db18feefa8ff280
fabianodsa <fabyanoduarte@hotmail.com>
2022-12-20 21:49:16 -0300

    função que adiciona os elementos no DOM criada

--- main.js
+++ main.js
@@ -2,6 +2,10 @@ const transactionsUl = document.querySelector('#transactions')
 const saldoReceita = document.querySelector('#saldoReceita')
 const saldoDespesa = document.querySelector('#saldoDespesa')
 const balanco = document.querySelector('#balance')
+const form = document.querySelector('#form')
+const inputNome = document.querySelector('#text')
+const inputValor = document.querySelector('#amount')
+
 
 
 const dummyTransactions = [
@@ -52,3 +56,29 @@ const init = () => {
 }
 
 init()
+
+const gerarID = () =>  Math.round(Math.random() * 1000)
+
+form.addEventListener('submit', event => {
+    event.preventDefault()
+
+    const transacaoNome = inputNome.value.trim()
+    const transacaoValor = inputValor.value.trim()
+
+    if (transacaoNome === '' || transacaoValor === '') {
+        alert("Preenha todos os campos!")
+        return
+    }
+
+    const transaction = { 
+        id: gerarID(), 
+        name: transacaoNome, 
+        amount: transacaoValor 
+    }
+    
+    dummyTransactions.push(transaction)
+    init()
+
+    inputNome.value = ''
+    inputValor.value = ''
+})
